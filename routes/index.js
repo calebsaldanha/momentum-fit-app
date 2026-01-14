debb80b3ce816a04fa4580dce00720e007961ab2
Caleb Saldanha <calebsaldanhawork@gmail.com>
2026-01-14 17:43:33 -0300

    Fix: Header EJS syntax error and remove padding on home

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // Renderiza a home passando o usuário para o header se adaptar (mostrar "Painel" em vez de "Entrar")
-    // Adicionado currentPage: 'index' para identificar página pública e remover sidebar/padding
     res.render('pages/index', {
         title: 'Início - Momentum Fit',
         user: req.session.user || null,
