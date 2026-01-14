f8385c865452da033cfc553a89d25284e5ad6e53
Caleb Saldanha <calebsaldanhawork@gmail.com>
2026-01-14 17:38:37 -0300

    Fix: Remove black space on homepage header and sidebar when logged in

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,19 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // Renderiza a home passando o usuário para o header se adaptar (mostrar "Painel" em vez de "Entrar")
+    // Adicionado currentPage: 'index' para identificar página pública e remover sidebar/padding
     res.render('pages/index', {
         title: 'Início - Momentum Fit',
-        user: req.session.user || null
+        user: req.session.user || null,
+        currentPage: 'index'
     });
 });
 
 router.get('/about', (req, res) => {
     res.render('pages/about', {
         title: 'Sobre - Momentum Fit',
-        user: req.session.user || null
+        user: req.session.user || null,
+        currentPage: 'about'
     });
 });
 
